cdb8ff33d355d639a9b26c3492b420302bc15bcb
BENDAHHANE Hajar <habendahhane@insea.ac.ma>
2025-07-17 16:23:38 +0100

    Fixed routes

--- index.js
+++ index.js
@@ -14,9 +14,14 @@ app.get('/api',function(req,res){
   var now = new Date();
   res.json({"unix":now.getTime(),"utc":now.toUTCString()});
 });
-app.get('/api/:date',function(req,res){
+app.get('/api/:date?',function(req,res){
   var input = req.params.date;
-  if(!isNaN(input)){
+  if(!input){
+    var now = new Date();
+    res.json({"unix":now.getTime(),"utc":now.toUTCString()});
+  }
+  else{
+    if(!isNaN(input)){
     var date=new Date(Number(input));
     res.json({"unix":Number(input),"utc":date.toUTCString()});
   }
@@ -25,7 +30,7 @@ app.get('/api/:date',function(req,res){
     if(!isNaN(date.getTime())){
     res.json({"unix":date.getTime(),"utc":date.toUTCString()});}
     else {res.json({error:"Invalid Date"});}
-  } 
+  }} 
 });
 
 app.get("/api/hello", function (req, res) {
